969ce94fdfcd5e814ac41125dd5bcb76f7dbd228
Liviu Popa <liviucst@gmail.com>
2024-06-26 23:58:28 +0300

    Updated the ReverseInteger app
    It was required to:
    - update the output (include parantheses for preceding zeroes)
    - refactor the functions (e.g. create dedicated function for capturing
      and pre-parsing input)

--- app/ReverseInteger/src/main.rs
+++ app/ReverseInteger/src/main.rs
@@ -1,44 +1,62 @@
 extern crate learn_rust_lib;
 use learn_rust_lib::numbers::reverse_int;
 
-fn pre_parse_input_string(number_as_str:&mut String) -> u8 {
-   let temp = number_as_str.trim().to_string();
-   *number_as_str = temp;
-
-   return number_as_str.len() as u8;
+fn clear_screen() {
+   std::process::Command::new("clear").status().unwrap();
 }
 
-fn parse_input_string(number_as_str:&String) -> Option<(u64,u8)> {
-   let mut result:Option<(u64,u8)> = Option::Some((0, 0));
+fn read_input() -> String {
+   let mut input_str = String::new();
+   std::io::stdin().read_line(&mut input_str).unwrap();
+   pre_parse_input_string(&mut input_str);
+   return input_str;
+}
+
+fn pre_parse_input_string(input_str:&mut String) {
+   let temp = input_str.trim().to_string();
+   *input_str = temp;
+}
+
+fn parse_input_string(input_str:&String) -> Option<(u64,u8)> {
+   if input_str.len() == 0 {
+      panic!("A non-empty input string is expected!");
+   }
+
+   let mut number_str = String::new();
    let mut preceding_zeroes_count = 0;
    let mut preceding_zeroes_check_required = true;
-   let mut number = String::new();
-   for ch in number_as_str.chars(){
+   let mut is_valid_string = true;
+
+   for ch in input_str.chars() {
       if !ch.is_digit(10) {
-         result = Option::None;
+         is_valid_string = false;
 	 break;
       }
-      else if preceding_zeroes_check_required {
+
+      if preceding_zeroes_check_required {
          if ch == '0' {
 	    preceding_zeroes_count += 1;
+	    continue;
 	 }
-	 else {
-	    number.push(ch);
-	    preceding_zeroes_check_required = false;
-	 }
+	 number_str.push(ch);
+	 preceding_zeroes_check_required = false;
       }
       else {
-         number.push(ch);
+         number_str.push(ch);
       }
    }
 
-   if result != Option::None {
-      if preceding_zeroes_count == number_as_str.len() as u8 {
-         result = Option::Some((0, preceding_zeroes_count - 1));
+   let mut result:Option<(u64,u8)> = Option::None;
+
+   if is_valid_string {
+      let mut number_int:u64 = 0;
+      if number_str.len() == 0 {
+         preceding_zeroes_count -= 1;
       }
       else {
-         result = Option::Some((number.parse::<u64>().unwrap(), preceding_zeroes_count));
+         number_int = number_str.parse::<u64>().unwrap();
       }
+      result = Option::Some((number_int, preceding_zeroes_count));
    }
 
    return result;
@@ -46,17 +64,17 @@ fn parse_input_string(number_as_str:&String) -> Option<(u64,u8)> {
 
 fn convert_prefixed_int_to_string(number:(u64, u8)) -> String {
    let mut result = String::new();
-   let mut zeroes_count = number.1;
-   while zeroes_count > 0 {
-      result.push('0');
-      zeroes_count -= 1;
+   if number.1 > 0 {
+      let mut zeroes_count = number.1;
+      result.push_str("(");
+      while zeroes_count > 0 {
+         result.push('0');
+         zeroes_count -= 1;
+      }
+      result.push_str(")");
    }
    result.push_str(number.0.to_string().as_str());
    return result;
-}
-
-fn clear_screen() {
-   std::process::Command::new("clear").status().unwrap();
 }
 
 fn main() {
@@ -68,25 +86,27 @@ fn main() {
 
    loop {
       println!("Please enter a positive integer (press ENTER to exit)!");
-      let mut number_as_str = String::new();
-      std::io::stdin().read_line(&mut number_as_str).unwrap();
+
+      let input_str = read_input();
+      let resulting_size = input_str.len();
+
       clear_screen();
-      let resulting_size = pre_parse_input_string(&mut number_as_str);
 
       if resulting_size == 0 {
          println!("Goodbye!");
 	 break;
       }
-      else if resulting_size <= max_digits_count {
-         let result = parse_input_string(&number_as_str);
+
+      if resulting_size <= max_digits_count {
+         let result = parse_input_string(&input_str);
          if result == Option::None {
             println!("Invalid integer! Please try again.")
          }
          else
          {
-             let unwrapped_result = result.unwrap();
-	     let reversed_result = reverse_int(unwrapped_result);
-	     println!("Reversed integer is: {}", convert_prefixed_int_to_string(reversed_result));
+            let unwrapped_result = result.unwrap();
+	    let reversed_result = reverse_int(unwrapped_result);
+	    println!("Reversed integer is: {}", convert_prefixed_int_to_string(reversed_result));
          }
       }
       else {

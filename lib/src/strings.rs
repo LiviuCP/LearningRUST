3324dbf26189078bfa764f4c04baa1342eaa17b3
Liviu Popa <liviucst@gmail.com>
2024-09-02 21:45:50 +0300

    Refactored the pig latin conversion functionality

--- lib/src/strings.rs
+++ lib/src/strings.rs
@@ -1,25 +1,25 @@
 use std::collections::HashSet;
 use super::utilities;
 
-/* Converts the string as follows:
-- if the string starts with a vowel it appends "-hay" to the end, e.g. "orange" becomes "orange-hay"
-- if the string starts with a consonant it takes the consonant and appends following substring to the end: "-[consonant]ay", e.g. "story" becomes "tory-say"
+fn is_starting_char_vowel(input_string_chars: &Vec::<char>) -> bool {
+    let mut is_vowel = false;
 
-Note: strings containing invalid characters (other than alpha-numeric) are discarded and an empty result is returned. Same is valid for empty strings or 1-char strings where the character is a consonant.
-*/
-pub fn convert_to_pig_latin(input_string: &String) -> String {
-    let input_string_chars: Vec::<char> = input_string.chars().collect();
-    let input_string_size = input_string_chars.len();
+    if input_string_chars.len() > 0 {
+	// for simplicity we only consider the "standard" latin vowels
+	is_vowel = HashSet::from(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']).contains(&input_string_chars[0])
+    }
+
+    is_vowel
+}
+
+fn is_valid_pig_latin_input(input_string_chars: &Vec::<char>) -> bool {
     let mut is_input_valid = false;
-    let mut is_starting_char_vowel = false;
-    let mut output_string_chars = Vec::<char>::new();
+    let input_string_size = input_string_chars.len();
 
     if input_string_size > 0 {
-	// for simplicity we only consider the "standard" latin vowels
-	is_starting_char_vowel = HashSet::from(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']).contains(&input_string_chars[0]);
-	is_input_valid = is_starting_char_vowel || input_string_size > 1;
-
 	// if the string size is exactly one character then this one should be a vowel
+	is_input_valid = input_string_size > 1 || is_starting_char_vowel(&input_string_chars);
+
 	if is_input_valid {
 	    for ch in input_string_chars.iter() {
 		if !ch.is_alphabetic() {
@@ -30,12 +30,29 @@ pub fn convert_to_pig_latin(input_string: &String) -> String {
 	}
     }
 
-    if is_input_valid {
-	// suffix has 4 characters ("-hay") but for consonants the second suffix char is the starting input string character so less space should be reserved
-	let suffix_space_to_reserve = if is_starting_char_vowel {4} else {3};
-	output_string_chars.reserve(input_string_size + suffix_space_to_reserve);
+    is_input_valid
+}
 
-	let copy_starting_index = if is_starting_char_vowel {0} else {1};
+/*
+Converts the string as follows:
+- if the string starts with a vowel it appends "-hay" to the end, e.g. "orange" becomes "orange-hay"
+- if the string starts with a consonant it takes the consonant and appends following substring to the end: "-[consonant]ay", e.g. "story" becomes "tory-say"
+
+Note: strings containing invalid characters (other than alpha-numeric) are discarded and an empty result is returned. Same is valid for empty strings or 1-char strings where the character is a consonant.
+*/
+pub fn convert_to_pig_latin(input_string: &String) -> String {
+    let input_string_chars: Vec::<char> = input_string.chars().collect();
+    let mut output_string_chars = Vec::<char>::new();
+
+    if is_valid_pig_latin_input(&input_string_chars) {
+	let is_first_input_char_vowel = is_starting_char_vowel(&input_string_chars);
+
+	// suffix has 4 characters ("-hay") but for consonants the second suffix char is the starting input string character so less space should be reserved
+	let suffix_space_to_reserve = if is_first_input_char_vowel {4} else {3};
+	output_string_chars.reserve(input_string_chars.len() + suffix_space_to_reserve);
+
+	// for consonants first char is copied when appending the suffix
+	let copy_starting_index = if is_first_input_char_vowel {0} else {1};
 
 	for ch in (&input_string_chars[copy_starting_index..]).iter() {
 	    let mut current_ch = *ch;
@@ -45,7 +62,7 @@ pub fn convert_to_pig_latin(input_string: &String) -> String {
 
 	let mut second_suffix_char = 'h';
 
-	if !is_starting_char_vowel {
+	if !is_first_input_char_vowel {
 	    second_suffix_char = input_string_chars[0];
 	    utilities::convert_char_to_lowercase(&mut second_suffix_char);
 	}

b596bd22808fdebce3bee954cca9398c621ff9c4
Liviu Popa <liviucst@gmail.com>
2024-09-30 22:35:23 +0300

    Created empty ConsList methods

---
+++ lib/src/conslists.rs
@@ -0,0 +1,24 @@
+enum ConsList<T> {
+    ConsValue(T, Box<ConsList<T>>),
+    Nil
+}
+
+impl<T> ConsList<T> {
+    pub fn create_from_vec(arr: &Vec<T>) -> ConsList<T> {
+	ConsList::<T>::Nil
+    }
+
+    pub fn append(&mut self, value: &T) {
+    }
+
+    pub fn reverse(&self) -> ConsList<T> {
+	ConsList::<T>::Nil
+    }
+
+    pub fn clear(&mut self) {
+    }
+
+    pub fn count(&self) -> usize {
+	0 as usize
+    }
+}

712453737844e10be2a14af2290d7c79ea4cb8d3
Liviu Popa <liviucst@gmail.com>
2024-10-15 17:20:24 +0300

    Added more cons creation tests

--- tests/src/consliststests.rs
+++ tests/src/consliststests.rs
@@ -1,8 +1,12 @@
 #[cfg(test)]
-use learn_rust_lib::conslists;
-use conslists::ConsList::{ConsValue, Nil};
+use learn_rust_lib::conslists::ConsList;
+use ConsList::{ConsValue, Nil};
 
 #[test]
 pub fn test_create_from_vec() {
-    assert_eq!(ConsValue(2, Box::new(ConsValue(5, Box::new(Nil)))), conslists::ConsList::<i32>::create_from_vec(&vec![2, 5]));
+    assert_eq!(Nil, ConsList::create_from_vec(&Vec::<i32>::new()));
+    assert_eq!(ConsValue(-3, Box::new(Nil)), ConsList::create_from_vec(&vec![-3]));
+    assert_eq!(ConsValue(2, Box::new(ConsValue(5, Box::new(Nil)))), ConsList::create_from_vec(&vec![2, 5]));
+    assert_eq!(ConsValue(-4, Box::new(ConsValue(8, Box::new(ConsValue(-3, Box::new(ConsValue(-3, Box::new(ConsValue(5, Box::new(ConsValue(0, Box::new(ConsValue(2, Box::new(ConsValue(1, Box::new(Nil)))))))))))))))),
+	       ConsList::create_from_vec(&vec![-4, 8, -3, -3, 5, 0, 2, 1]));
 }

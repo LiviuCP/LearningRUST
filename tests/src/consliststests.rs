b5c64fd4a43311b17f05f8a3e1fa71c73c8f16ae
Liviu C. Popa <liviucst@gmail.com>
2024-10-16 23:24:31 +0300

    Refactored test_prepend()

--- tests/src/consliststests.rs
+++ tests/src/consliststests.rs
@@ -37,29 +37,17 @@ pub fn test_prepend() {
     let mut value = -3;
     wrapper.prepend(&value);
 
-    assert!(*wrapper.value() == ConsValue(Rc::new(Rcl::new(-3)), Rc::new(Nil)) && wrapper.size() == 1);
+    assert!(*wrapper.value() == *ConsWrapper::create_from_vec(&vec![-3]).value() && wrapper.size() == 1);
 
     wrapper = ConsWrapper::create_from_vec(&vec![5]);
     value = 2;
     wrapper.prepend(&value);
 
-    assert!(*wrapper.value() == ConsValue(Rc::new(Rcl::new(2)),
-					   Rc::new(ConsValue(Rc::new(Rcl::new(5)),
-							     Rc::new(Nil)))) &&
-	    wrapper.size() == 2);
+    assert!(*wrapper.value() == *ConsWrapper::create_from_vec(&vec![2, 5]).value() && wrapper.size() == 2);
 
     wrapper = ConsWrapper::create_from_vec(&vec![8, -3, -3, 5, 0, 2, 1]);
     value = -4;
     wrapper.prepend(&value);
 
-    assert!(*wrapper.value() == ConsValue(Rc::new(Rcl::new(-4)),
-					  Rc::new(ConsValue(Rc::new(Rcl::new(8)),
-							    Rc::new(ConsValue(Rc::new(Rcl::new(-3)),
-									      Rc::new(ConsValue(Rc::new(Rcl::new(-3)),
-												Rc::new(ConsValue(Rc::new(Rcl::new(5)),
-														  Rc::new(ConsValue(Rc::new(Rcl::new(0)),
-																    Rc::new(ConsValue(Rc::new(Rcl::new(2)),
-																		      Rc::new(ConsValue(Rc::new(Rcl::new(1)),
-																					Rc::new(Nil)))))))))))))))) &&
-	    wrapper.size() == 8);
+    assert!(*wrapper.value() == *ConsWrapper::create_from_vec(&vec![-4, 8, -3, -3, 5, 0, 2, 1]).value() && wrapper.size() == 8);
 }

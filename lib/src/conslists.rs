040687036f84007eedf1edea9dbf78da62618332
Liviu Popa <liviucst@gmail.com>
2024-10-16 09:34:00 +0300

    Created ConsWrapper for managing ConsList

--- lib/src/conslists.rs
+++ lib/src/conslists.rs
@@ -2,6 +2,36 @@
 TODO: create (mut) iterators
 */
 
+pub struct ConsWrapper<T> {
+    list: Box<ConsList<T>>,
+    count: usize
+}
+
+impl<T: Copy> ConsWrapper<T> {
+    pub fn create_from_vec(arr: &Vec<T>) -> ConsWrapper<T> {
+	let mut result = ConsWrapper::<T>{list: Box::new(ConsList::<T>::Nil), count: 0};
+
+	for val in arr.iter().rev() {
+	    result.list = Box::new(ConsList::ConsValue(*val, result.list));
+	    result.count += 1;
+	}
+	
+	result
+    }
+
+    pub fn value(&self) -> &ConsList::<T> {
+	&self.list
+    }
+
+    pub fn size(&self) -> usize {
+	self.count
+    }
+
+    pub fn empty(&self) -> bool {
+	self.count == 0
+    }
+}
+
 #[derive(PartialEq, Debug, Clone)]
 pub enum ConsList<T> {
     ConsValue(T, Box<ConsList<T>>),
@@ -9,14 +39,6 @@ pub enum ConsList<T> {
 }
 
 impl<T: Copy> ConsList<T> {
-    pub fn create_from_vec(arr: &Vec<T>) -> ConsList<T> {
-	let mut result = ConsList::<T>::Nil;
-	for val in arr.iter().rev() {
-	    result = ConsList::ConsValue(*val, Box::new(result));
-	}
-	result
-    }
-
     pub fn prepend(&mut self, value: &T) {
 	*self = ConsList::ConsValue(*value, Box::new(self.clone()));
     }
--- tests/src/consliststests.rs
+++ tests/src/consliststests.rs
@@ -1,14 +1,21 @@
 #[cfg(test)]
-use learn_rust_lib::conslists::ConsList;
+use learn_rust_lib::conslists::{ConsList, ConsWrapper};
 use ConsList::{ConsValue, Nil};
 
 #[test]
 pub fn test_create_from_vec() {
-    assert_eq!(Nil, ConsList::create_from_vec(&Vec::<i32>::new()));
-    assert_eq!(ConsValue(-3, Box::new(Nil)), ConsList::create_from_vec(&vec![-3]));
-    assert_eq!(ConsValue(2, Box::new(ConsValue(5, Box::new(Nil)))), ConsList::create_from_vec(&vec![2, 5]));
-    assert_eq!(ConsValue(-4, Box::new(ConsValue(8, Box::new(ConsValue(-3, Box::new(ConsValue(-3, Box::new(ConsValue(5, Box::new(ConsValue(0, Box::new(ConsValue(2, Box::new(ConsValue(1, Box::new(Nil)))))))))))))))),
-	       ConsList::create_from_vec(&vec![-4, 8, -3, -3, 5, 0, 2, 1]));
+    let mut wrapper = ConsWrapper::create_from_vec(&Vec::new());
+    assert!(*wrapper.value() == Nil && wrapper.empty());
+
+    wrapper = ConsWrapper::create_from_vec(&vec![-3]);
+    assert!(*wrapper.value() == ConsValue(-3, Box::new(Nil)) && wrapper.size() == 1);
+
+    wrapper = ConsWrapper::create_from_vec(&vec![2, 5]);
+    assert!(*wrapper.value() == ConsValue(2, Box::new(ConsValue(5, Box::new(Nil)))) && wrapper.size() == 2);
+
+    wrapper = ConsWrapper::create_from_vec(&vec![-4, 8, -3, -3, 5, 0, 2, 1]);
+    assert!(*wrapper.value() == ConsValue(-4, Box::new(ConsValue(8, Box::new(ConsValue(-3, Box::new(ConsValue(-3, Box::new(ConsValue(5, Box::new(ConsValue(0, Box::new(ConsValue(2, Box::new(ConsValue(1, Box::new(Nil)))))))))))))))) &&
+	    wrapper.size() == 8);
 }
 
 #[test]
